47fee88c7bdcfd121daadf538968b233dc6f4813
James <907310116@qq.com>
2023-05-09 17:24:33 +0800

    修改标题以及标题图片

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header-wrapper">
-    <span class="header-title">车型定制化平台
+    <span class="header-title">车展厅服务平台
     </span>
   </div>
 </template>
@@ -15,12 +15,12 @@
   right:0px;
   width:100%;
   height:60px;
-  background-image: url('@/assets/images/layout/header.png');
+  background-image: url('@/assets/images/layout/header_img.png');
   background-position: center 0;
   background-repeat: no-repeat;
   .header-title{
     position: absolute;
-    top:10px;
+    top:2px;
     left:50%;
     transform: translateX(-50%);
     font-size:2rem;
